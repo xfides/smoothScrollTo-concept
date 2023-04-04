34e5821590afea280e049f88d830bbf276482e62
nat-davydova <natti.davydova@gmail.com>
2023-04-04 18:08:23 +0300

    feat: calc start scroll position

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
+const DOM = {
+  nav: "navigation",
+  navLink: "navigation__link",
+};
 
+const navigation = document.querySelector(`.${DOM.nav}`);
+
+export function smoothScrollTo() {
+  // сначала он отрабатывает, только потом переходит по якорю
+  const startPosition = window.scrollY;
+  console.log(startPosition);
+}
+
+navigation?.addEventListener("click", (e) => {
+  const currentTarget = e.target as HTMLElement;
+
+  if (!currentTarget.closest(`.${DOM.navLink}`)) {
+    return;
+  }
+
+  console.log(currentTarget);
+
+  smoothScrollTo();
+});
